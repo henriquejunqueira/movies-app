d9f8d983e81c5cb5d0cd5a084c5a038459f256f1
Henrique Junqueira <41648535+henriquejunqueira@users.noreply.github.com>
2025-01-10 13:12:16 -0300

    Criado projeto, estruturado e estilizado

---
+++ src/components/MovieList/index.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import './index.scss';
+import axios from 'axios';
+
+export default function MovieList() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  const getMovies = () => {
+    axios({
+      method: 'get',
+      url: 'https://api.themoviedb.org/3/discover/movie',
+      params: {
+        api_key: '66735d242e135ef82b382c9f343ddafd',
+        language: 'pt-BR',
+      },
+    }).then((response) => {
+      setMovies(response.data.results);
+    });
+  };
+
+  return (
+    <ul className='movie-list'>
+      {movies.map((movie) => (
+        <li></li>
+      ))}
+    </ul>
+  );
+}
